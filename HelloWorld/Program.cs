7cb6e8b3935c45a0b67fc870c6edda37b7be9877
Matthew Wareing <Matthew.Wareing@education.nsw.gov.au>
2017-10-12 12:39:22 +1100

    creted welcome message and hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,9 +10,25 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.Title = "my program";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("HelloWorld");
-            Console.ReadLine();
-            Console.WriteLine()
+            Console.ReadKey();
+            Console.WriteLine("damn son you uuuuuuugly");
+            Console.ReadKey();
+            Console.Beep(400, 500);
+            Console.Beep(400, 500);
+            Console.Beep(400, 500);
+            System.Threading.Thread.Sleep(500);
+            Console.Beep(600, 1000);
+            Console.Beep(500, 1000);
+            Console.Beep(700, 1000);
+            Console.Beep(600, 1000);
+            Console.Beep(500, 1000);
+            Console.Beep(700, 1000);
+
+
+
         }
     }
 }
---
+++ welcome message/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace welcome_message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter last name:");
+            string lastname = Console.ReadLine();
+            Console.Write("enter first name:");
+            string firstname = Console.ReadLine();
+            Console.WriteLine($"Welcome to the program {firstname} {lastname}");
+
+
+
+
+            //wait at end
+            Console.ReadKey();
+        }
+    }
+}
