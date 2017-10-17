598140ddfabb792bb7485d6072d7933569a4eda1
Matthew Wareing <Matthew.Wareing@education.nsw.gov.au>
2017-10-18 10:30:12 +1100

    made pick a door program

--- pick a door/Program.cs
+++ pick a door/Program.cs
@@ -10,6 +10,36 @@ namespace pick_a_door
     {
         static void Main(string[] args)
         {
+
+            //input
+
+
+            Console.WriteLine("Pick a door 1 , 2 , or 3");
+            string selection = Console.ReadLine();
+
+            //output
+
+
+            if (selection == "1")
+            {
+                Console.WriteLine("behind door 1 is a didgerdildo ");
+            }
+            else if (selection == "2")
+            {
+                Console.WriteLine("behind door 2 is asscancer you have been infected");
+            }
+            else if (selection == "3")
+            {
+                Console.WriteLine("behind door 3 is the coopers dignity. He lost it a while ago.");
+            }
+            else
+            {
+                Console.WriteLine("that aint a door bitch");
+            }
+
+
+            //wait at end
+            Console.ReadLine();
         }
     }
 }
