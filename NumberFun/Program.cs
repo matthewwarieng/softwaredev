cda8c1381658c2d214bd785cf65931bd57b9d8eb
Matthew Wareing <Matthew.Wareing@education.nsw.gov.au>
2017-10-18 09:48:22 +1100

    made it read input as numbers it float

---
+++ NumberFun/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+
+                Console.WriteLine("enter number X") ;
+                float X = float.Parse( Console.ReadLine());
+                Console.WriteLine("enter number Y") ;
+                float Y = float.Parse(Console.ReadLine()) ;
+
+
+            //output
+
+                Console.WriteLine("X plus Y = " + (X + Y))  ;
+                Console.WriteLine("X times Y = " + (X * Y)) ;
+                Console.WriteLine("X minus Y = " + (X - Y)) ;
+                Console.WriteLine("X divided by Y = " + (X / Y)) ;
+
+            //Wait at end
+            Console.ReadLine();
+        }
+    }
+}
