9e3a87c839dd2f0ed261f7c8d5eed550eb69d46f
Matthew Wareing <Matthew.Wareing@education.nsw.gov.au>
2017-10-24 14:57:26 +1100

    loops

---
+++ Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+
+
+            Console.WriteLine("ENTER number:");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            int num= int.Parse(Console.ReadLine());
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            //OUTPUT
+
+            for (int x = 1; x <= num; x++) 
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("press enter to see while:");
+            Console.ReadLine();
+
+            int num2 = 1;
+
+            while (num2 <= 10)
+            {
+                Console.WriteLine(num2);
+                num2 ++;
+            }
+
+
+            //wait at end
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Loopsendless/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loopsendless
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+
+
+            Console.WriteLine("ENTER number:");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            int num = int.Parse(Console.ReadLine());
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            //OUTPUT
+
+            for (int x = 1; ; x++ )
+            {
+                Console.Write(x);
+            }
+
+            if (x = ) ;
+        }
+    }
+}
