68d8d245a64a604269eb27b525f5c9583d7d7bfa
Matthew Wareing <Matthew.Wareing@education.nsw.gov.au>
2017-11-15 10:43:53 +1100

    test probability version

--- BabyBlackJack/Program.cs
+++ BabyBlackJack/Program.cs
@@ -10,31 +10,40 @@ namespace BabyBlackJack
     {
         static void Main(string[] args)
         {
-            float money = 10;
+            float money = 10;            
             playGame(ref money);
 
         }
 
         private static void playGame(ref float money)
         {
-            Console.Clear();
-            float betAmount = MakeBet(ref money);
-            Random rnd = new Random();
-            int playerTotal = DealCards(rnd, "You");
-            int dealerTotal = DealCards(rnd, "The Dealer");
-            bool PlayerWins = determineWinner(playerTotal, dealerTotal, betAmount);
-            PayOut(PlayerWins, betAmount, ref money);
-            menu(ref money);
+            int wins = 0;
+            for (int i = 1; i <= 362880; i++)
+            {
+                Console.Clear();
+                float betAmount = 0;
+                Random rnd = new Random();
+                int playerTotal = DealCards(rnd, "You");
+                int dealerTotal = DealCards(rnd, "The Dealer");
+                bool PlayerWins = determineWinner(playerTotal, dealerTotal, betAmount);
+                if (PlayerWins) wins++;
+                PayOut(PlayerWins, betAmount, ref money);
+            }
+
+            float percentage = (float)wins / (float)362880;
+            Console.WriteLine($"player wins {percentage * 100}% of the time");
+            Console.ReadKey();
+            //menu(ref money, count);
         }
 
         private static void menu(ref float money)
         {
-            Console.WriteLine("Would you like to play another game? yes/no; ");
-            string choice = Console.ReadLine();
-            if (choice == "y" || choice == "yes")
-            {
+            //Console.WriteLine("Would you like to play another game? yes/no; ");
+            //string choice = Console.ReadLine();
+            //if (choice == "y" || choice == "yes")
+            //{
                 playGame(ref money);
-            }
+            //}
         }
 
         private static void PayOut(bool playerWins, float betAmount, ref float money)
@@ -47,43 +56,43 @@ namespace BabyBlackJack
 
         private static bool determineWinner(int playerTotal, int dealerTotal, float betAmount)
         {
-            System.Threading.Thread.Sleep(750);
+            //System.Threading.Thread.Sleep(750);
 
             if (playerTotal > dealerTotal)
             {
-                Console.WriteLine($"You Won ${betAmount * 2}");
+                //Console.WriteLine($"You Won ${betAmount * 2}");
                 return true;
             }
 
             else if (playerTotal < dealerTotal)
             {
-                Console.WriteLine($"You Lost ${betAmount * 2}");
+                //Console.WriteLine($"You Lost ${betAmount * 2}");
                 return false;
             }
 
             else
             {
-                Console.WriteLine("Its a Draw, Dealers favor, Dealer Wins. ");
+                //Console.WriteLine("Its a Draw, Dealers favor, Dealer Wins. ");
                 return false;
             }
         }
 
         private static int DealCards(Random rnd, string Player)
         {
-            Console.Write("dealing cards");
-            System.Threading.Thread.Sleep(750);
-            Console.Write(".");
-            System.Threading.Thread.Sleep(750);
-            Console.Write(".");
-            System.Threading.Thread.Sleep(750);
-            Console.Write(".");
+            //Console.Write("dealing cards");
+            //System.Threading.Thread.Sleep(750);
+            //Console.Write(".");
+            //System.Threading.Thread.Sleep(750);
+            //Console.Write(".");
+            //System.Threading.Thread.Sleep(750);
+            //Console.Write(".");
 
-            Console.WriteLine("");
+            //Console.WriteLine("");
 
             int Card1 = rnd.Next(1, 11);
             int Card2 = rnd.Next(1, 11);
             int Total = Card1 + Card2;
-            Console.WriteLine($"{Player} drew {Card1} and {Card2} making a total of {Total} ");
+            //Console.WriteLine($"{Player} drew {Card1} and {Card2} making a total of {Total} ");
             return Total;
         }
 
@@ -91,20 +100,20 @@ namespace BabyBlackJack
         {
             float betAmount = 0;
             bool validBet = false;
-            Console.WriteLine($"Enter your bet. You currently have ${money}");
+           // Console.WriteLine($"Enter your bet. You currently have ${money}");
             while (validBet != true)
             {
                 validBet = float.TryParse(Console.ReadLine(), out betAmount);
 
                 if (betAmount > money)
                 {
-                    Console.WriteLine($"sorry you do not have enough money to make this bet. You only have {money}, please try again.");
+                   // Console.WriteLine($"sorry you do not have enough money to make this bet. You only have {money}, please try again.");
                     validBet = false;
                 }
 
                 else if (betAmount < 0)
                 {
-                    Console.WriteLine("Bet can not be a negative number, Please try again.");
+                    //Console.WriteLine("Bet can not be a negative number, Please try again.");
                     validBet = false;
                 }
             }
